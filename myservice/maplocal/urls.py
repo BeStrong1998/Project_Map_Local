a7f85d135bbcfd00a4cfdb9cd689be8b20521c92
BeStrong1998 <BeStrong1998>
2023-04-27 14:58:53 +0300

    urls and view

--- myservice/maplocal/urls.py
+++ myservice/maplocal/urls.py
@@ -1,19 +1,26 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from .views import *
 
 
 
 router = routers.DefaultRouter()
-router.register(r'citys', views.CityViewSet)
-router.register(r'streets', views.StreetViewSet)
-router.register(r'markets', views.MarketViewSet)
+router.register(r'citys', views.CitysViewSet) #Коллекция citys
+router.register(r'streets', views.StreetsViewSet) #коллекция streets
+router.register(r'markets', views.MarketsViewSet) #коллекция markets
 
 urlpatterns = [
-    #path('', views.index, name='index'),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
+    path('city/', CityViewSet.as_view()),  #Колекция объекта город
+    path('city/<int:pk>/', CityDetail.as_view()), #один бъект из колекции по id
+    path('city/create/', CityCreateView.as_view()), #для создания нового объекта
+    path('city/<int:city_id>/street/', StreetViewset.as_view()), #все улицы одного города
+    path('street/create/', StreetCreateView.as_view()), #для создания улиц
+    path('market/', MarketViewSet.as_view()), #Колекция объекта магазин
+    path('market/create/', MarketCreateView.as_view()), #для создания магазинов
 ]
 
 
--- myservice/maplocal/views.py
+++ myservice/maplocal/views.py
@@ -1,26 +1,79 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 from rest_framework import viewsets
-
+from rest_framework import generics
 from .serializers import CitySerializer, StreetSerializer, MarketSerializer
 from .models import City, Street, Market
+from rest_framework.response import Response
 
 
 
 """def index(request):
     return HttpResponse("Hello, world!")"""
 
-class CityViewSet(viewsets.ModelViewSet):
-    queryset = City.objects.all().order_by('city')
+class CitysViewSet(viewsets.ModelViewSet): #колекция городов
+    queryset = City.objects.all()#.order_by('city')
     serializer_class = CitySerializer
 
 
-class StreetViewSet(viewsets.ModelViewSet):
-    queryset = Street.objects.all().order_by('street')
+class StreetsViewSet(viewsets.ModelViewSet): #колекция улиц
+    queryset = Street.objects.all()#.order_by('street')
     serializer_class = StreetSerializer
 
 
-class MarketViewSet(viewsets.ModelViewSet):
-    queryset = Market.objects.all().order_by('name')
+class MarketsViewSet(viewsets.ModelViewSet): #колекция магазинов
+    queryset = Market.objects.all()#.order_by('name')
+    serializer_class = MarketSerializer
+
+
+
+
+class CityViewSet(generics.ListAPIView): #Берём всю коллекцию оюъекта
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+    def handle(self, ex):
+        return Response({'data': 'Коллекция пуста'}, status=400)
+
+
+class CityDetail(generics.RetrieveAPIView): #Берём один объект из колекции
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+    def handle(self, ex):
+        return Response({'data': 'Нету такого города с указанным city_id'}, status=400)
+
+
+class CityCreateView(generics.CreateAPIView): #Создаём новый объект
+    serializer_class = CitySerializer
+    
+    def handle(self, ex):
+        return Response({'data': 'Добавьте город'}, status=400)
+    
+
+class StreetViewset(generics.ListAPIView):
+    serializer_class = StreetSerializer
+
+    def get_queryset(self):
+        queryset = Street.objects.all()
+        city_id = self.kwargs['city_id']
+        if city_id is not None:
+            queryset = queryset.filter(city_id=city_id)
+        return queryset
+    
+
+class StreetCreateView(generics.CreateAPIView):
+    serializer_class = StreetSerializer
+
+    def handle(self, ex):
+        return Response({'data': 'Добавьте улицу'}, status=400)
+
+
+class MarketCreateView(generics.CreateAPIView):
+    serializer_class = MarketSerializer
+
+    def handle(self, ex):
+        return Response({'data': 'Добавьте магазин'}, status=400)
+    
+
+class MarketViewSet(generics.ListAPIView):
+    queryset = Market.objects.all()
     serializer_class = MarketSerializer
